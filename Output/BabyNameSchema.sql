a3ba5e743c261cd361b1c545ad94e4b96033fa91
DataNinjaPrincess <79807487+DataNinjaPrincess@users.noreply.github.com>
2021-06-16 19:11:57 -0400

    Adding SQL Schema and ERD

---
+++ Output/BabyNameSchema.sql
@@ -0,0 +1,24 @@
+-- Drop table if exists
+DROP TABLE OlympicWinners;
+-- DROP TABLE BabyNames;
+
+CREATE TABLE OlympicWinners (
+	WinYear integer,
+	City text,
+    Discipline text,
+	Sport text,
+	Athlete text,
+	Country text,
+	Gender text,
+	WinningEvent text,
+	Medal text,
+	Season text,
+	FirstName text
+	);
+	
+CREATE TABLE BabyNames (
+	FirstName text,
+ 	NameYear integer,
+    Frequency integer,
+ 	Primary Key (NameYear, FirstName),
+ 	);
